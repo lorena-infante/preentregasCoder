f7d455f52e824edaa1524630a1a666f626d87368
Lorena Infante <m.lorenainfanter@gmail.com>
2023-11-22 22:26:11 -0300

    added constructor classes, app.js with new array of objects, ready to instantiate a new class

--- preentrega2_Infante_Lorena/app.js
+++ preentrega2_Infante_Lorena/app.js
@@ -1,1 +1,77 @@
-alert('hello');
+//categorización
+const categorias = [
+    {
+        parras: {
+            moscatel: {
+                nombre_prod: "moscatel",
+                precio_prod: 8000,
+                altura: '1mt',
+                img: './moscatel.jpg',
+                descripcion: 'Uva de Chile',
+                cuidados: 'Regarla',
+                iluminacion: [sol],
+                stock: 10,
+                temporada: "Otoño/Primavera"
+            },
+            sultanina: {
+                nombre_prod: "sultanina",
+                precio_prod: 7000,
+                altura: '1mt',
+                img: './sultanina.jpg',
+                descripcion: 'Uva de Chile',
+                cuidados: 'Regarla',
+                iluminacion: [sol],
+                stock: 10,
+                temporada: "Otoño/Primavera"
+            },
+            torontel: {
+                nombre_prod: "torontel",
+                precio_prod: 9000,
+                altura: '1mt',
+                img: './torontel.jpg',
+                descripcion: 'Uva de Chile',
+                cuidados: 'Regarla',
+                iluminacion: [sol],
+                stock: 10,
+                temporada: "Otoño/Primavera"
+            }
+        },
+        frutales: {
+            almendro: {
+                nombre_prod: "almendro",
+                precio_prod: 9000,
+                altura: '1,5mts',
+                img: './almendro.jpg',
+                descripcion: 'Almendro de Chile',
+                cuidados: 'Regarla',
+                iluminacion: [sol, semi - sombra],
+                stock: 10,
+                temporada: "Otoño/Primavera"
+            },
+            arandano: {
+                nombre_prod: "arandano",
+                precio_prod: 8000,
+                altura: '60cms',
+                img: './arandano.jpg',
+                descripcion: 'Arándano de Chile',
+                cuidados: 'Regarla',
+                iluminacion: [semi - sombra],
+                stock: 10,
+                temporada: "Otoño/Primavera"
+            },
+            caqui: {
+                nombre_prod: "caqui",
+                precio_prod: 9000,
+                altura: '1mt',
+                img: './caqui.jpg',
+                descripcion: 'Caqui de Chile',
+                cuidados: 'Regarla',
+                iluminacion: [semi - sombra],
+                stock: 10,
+                temporada: "Otoño/Primavera"
+            }
+        }
+    }
+];
+
+//instanciación de la clase
---
+++ preentrega2_Infante_Lorena/classes.js
@@ -0,0 +1,22 @@
+//clase constructora
+class Producto {
+        constructor(nombre,precio,altura,
+            img,descripcion,cuidados,iluminacion, stock,temporada){
+                this.nombre = nombre;
+                this.precio = precio;
+                this.altura = altura;
+                this.img = img;
+                this.descripcion = descripcion;
+                this.cuidados = cuidados;
+                this.iluminacion = iluminacion;
+                this.stock = stock;
+                this.temporada = temporada;
+        }
+        verDetallesProducto(){
+            console.log(`El producto ${this.nombre} tiene las siguientes características: - Descripción: ${this.descripcion}.
+            - Cuidados: ${this.cuidados}.
+            - Iluminación: ${this.iluminacion}.
+            - Precio: ${this.precio}.
+            `);
+        }
+}
